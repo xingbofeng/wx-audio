523603c8944cda20d7d6e7688e7bb98ce03c9fd2
FE_XingBofeng <encounterxingbofeng@outlook.com>
2017-01-21 17:49:51 +0800

    feat: use nodejs to format url

---
+++ server/formatURL.js
@@ -0,0 +1,17 @@
+const URL = function(musicname) {
+	let url = 'http://s.music.163.com/search/get/';
+	const data = {
+		'type': '1',
+		'limit': '1',
+		's': musicname,
+	};
+	for (let key in data) {
+		if (key === 'type') {
+			url += `?${key}=${encodeURIComponent(data[key])}`;
+		} else {
+			url += `&${key}=${encodeURIComponent(data[key])}`;
+		}
+	}
+	return url;
+}
+module.exports = URL;
--- server/index.js
+++ server/index.js
@@ -2,21 +2,21 @@ var express = require('express');
 var bodyParser = require('body-parser');
 // 使用body-parser解析post请求的参数，如果没有，req.body为undefined。
 var http = require('http');
+var formatURL = require('./formatURL.js');
 var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.post('/', (req, respon) => {
-	console.log(req.body);
 	var body = '';
 	// http模块拿到真实后台api的数据
-	http.get(req.body.url, function(res) {
+	// console.log(formatURL);
+	http.get(formatURL(req.body.musicname), function(res) {
 		res.on('data', (data) => {
 			body += data;
 		}).on('end', () => {
 			respon.send(JSON.parse(body));
 		})
 	});
-	console.log(body);
 });
 app.listen(3000, () => {
 	console.log('open wx-audio server successful!')
